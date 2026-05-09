3940a6d4daeee53202e68b8fbf7c25bf09c7fac3
Bryan <gautintin303@gmail.com>
2026-05-09 13:56:32 -0700

    idk v2

---
+++ amazing-game/modules/cards/cards.gd
@@ -0,0 +1,78 @@
+class_name Cards
+
+extends Area2D
+
+signal card_played(clicked_card: Cards, player_id: int, type: CardType)
+
+enum CardType {DAMAGE, SPEED, HEALTH}
+
+@export var damage_texture: Texture2D
+@export var speed_texture: Texture2D
+@export var health_texture: Texture2D
+
+@export var player_owner: int
+
+@export var card_type: CardType
+
+var drop_distance = 800.0
+
+var on_screen_position: Vector2
+
+var off_screen_position: Vector2
+
+var is_selectable = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	match card_type:
+		CardType.DAMAGE:
+			$Sprite2D.texture = damage_texture
+		CardType.SPEED:
+			$Sprite2D.texture = speed_texture
+		CardType.HEALTH:
+			$Sprite2D.texture = health_texture
+	
+	on_screen_position = position
+	
+	position.y -= drop_distance
+	
+	off_screen_position = position
+	
+	var tween = create_tween()
+	tween.tween_property(self, "position", on_screen_position, 2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	
+	await tween.finished
+	
+	is_selectable = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if (is_selectable == false):
+		return
+	
+	if (event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT):
+		select_card()
+		is_selectable = false
+
+func select_card() -> void:
+	card_played.emit(self, player_owner, card_type)
+	
+	var tween = create_tween()
+	tween.tween_property(self, "position", off_screen_position, 3.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	
+	await tween.finished
+	
+	queue_free()
+
+func discard() -> void:
+	is_selectable = false
+	
+	var tween = create_tween()
+	tween.tween_property(self, "position", off_screen_position, 1.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	
+	await tween.finished
+	
+	queue_free()
---
+++ amazing-game/scenes/select_cards/select_cards.gd
@@ -0,0 +1,62 @@
+extends CanvasLayer
+
+signal upgrades_finished
+
+@export var card_scene: PackedScene
+@export var card_spacing: float = 100.0
+
+var p1_has_played: bool = false
+var p2_has_played: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("Cards")
+	
+	var screen_size = get_viewport().get_visible_rect().size
+	
+	$SpawnPoint1.global_position = Vector2(screen_size.x / 4.0, screen_size.y / 2.0)
+	$SpawnPoint2.global_position = Vector2(screen_size.x * 3.0 / 4.0, screen_size.y / 2.0)
+	
+	var starting_cards = [Cards.CardType.DAMAGE, Cards.CardType.HEALTH, Cards.CardType.SPEED]
+	spawn_hand(1, $SpawnPoint1.global_position, starting_cards)
+	spawn_hand(2, $SpawnPoint2.global_position, starting_cards)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawn_hand(player_id: int, center_pos: Vector2, card_types: Array) -> void:
+	var hand_size = card_types.size()
+	
+	for i in range(hand_size):
+		var card = card_scene.instantiate()
+		
+		var offset_x = (i - (hand_size - 1) / 2.0) * card_spacing
+		card.position = center_pos + Vector2(offset_x, 0)
+		
+		card.player_owner = player_id
+		card.card_type = card_types[i]
+		
+		card.card_played.connect(_on_card_played)
+		
+		add_child(card)
+
+func _on_card_played(clicked_card: Cards, player_id: int, type: Cards.CardType) -> void:
+	if (player_id == 1):
+		p1_has_played = true
+	else:
+		p2_has_played = true
+	
+	for child in get_children():
+		if child is Cards and child.player_owner == player_id and child != clicked_card:
+			child.discard()
+	
+	if (p1_has_played and p2_has_played):
+		end_selection_phase()
+
+func end_selection_phase() -> void:
+	await get_tree().create_timer(3.0).timeout
+	
+	upgrades_finished.emit()
+	
+	queue_free()
